1828fccf25e5333824074fc52824801c6d6281bc
Kaloyan <konik98@gmail.com>
2020-11-25 19:10:33 +0100

    [Add] Finished 2.2C
    Added the provided dependencies.

---
+++ XRHarvest/Assets/HarvestSupplementaryMaterial/Scripts/GlobalFallCleaner.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GlobalFallCleaner : MonoBehaviour
+{
+    private BoxCollider boxCollider;
+
+    private void Start()
+    {
+        boxCollider = gameObject.AddComponent<BoxCollider>();
+        boxCollider.size = new Vector3(100, 0.4f, 100);
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        Destroy(collision.gameObject);
+    }
+}
